3932412fd3e3c11a4a173145580619973cfe2502
PRO-Coding <80043809+pro-whitehatjr@users.noreply.github.com>
2021-10-11 18:19:25 +0530

    Add files via upload

---
+++ CeaserCryptography.py
@@ -0,0 +1,34 @@
+
+print("Welcme to the world of cryptography")
+
+def main():
+    print()
+    print("Choose one option")
+    choice = int(input("1. Encryption\n2. Decryption\nChoose(1,2): "))
+    if choice == 1:
+        encryption()
+    elif choice == 2:
+        decryption()
+    else:
+        print("Wrong Choice")
+
+
+def encryption():
+
+
+
+    
+
+
+def decryption():
+
+
+
+
+
+    
+    
+
+        
+if __name__ == "__main__":
+    main()
